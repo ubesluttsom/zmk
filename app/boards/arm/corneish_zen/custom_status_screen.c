e34fe8134f374757928d6a4d0d804c499f1a8617
Martin Mihle Nygaard <martin@mihle.no>
2023-11-23 02:08:10 +0100

    fix: alignment of peripheral status widget

--- app/boards/arm/corneish_zen/custom_status_screen.c
+++ app/boards/arm/corneish_zen/custom_status_screen.c
@@ -51,7 +51,7 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
     lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_MID, 0,
-                 41);
+                 0);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
