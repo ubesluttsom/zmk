de2c83da5e567e1c433e7e293d11e4b133d293c0
Martin Mihle Nygaard <martin@mihle.no>
2023-11-23 13:53:00 +0100

    Have Mick as "zenlogo" fullscreen background

--- app/boards/arm/corneish_zen/custom_status_screen.c
+++ app/boards/arm/corneish_zen/custom_status_screen.c
@@ -38,10 +38,12 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
-    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_MID, 0,
-                 20);
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    /* Logo first, to use as "background" */
+    lv_obj_t *zenlogo_icon;
+    zenlogo_icon = lv_img_create(screen);
+    lv_img_set_src(zenlogo_icon, &zenlogo);
+    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
@@ -54,7 +56,14 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 41);
 #endif
 
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_MID, 0,
+                 41);
+#endif
+
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
+    /* Disable static "LAYER" header */
     /* lv_obj_t *LayersHeading; */
     /* LayersHeading = lv_img_create(screen); */
     /* lv_obj_align(LayersHeading, LV_ALIGN_BOTTOM_MID, 0, -30); */
@@ -65,15 +74,6 @@ lv_obj_t *zmk_display_status_screen() {
                                LV_PART_MAIN);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
 #endif
-
-    /* Logo is commented out for now. Picture is displayed though Bluetooth connected logo instead.
-     */
-    /* #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL) */
-    /*     lv_obj_t *zenlogo_icon; */
-    /*     zenlogo_icon = lv_img_create(screen); */
-    /*     lv_img_set_src(zenlogo_icon, &zenlogo); */
-    /*     lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5); */
-    /* #endif */
 
     return screen;
 }
